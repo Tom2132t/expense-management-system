46a9d3de412fe2b0e4653ba762b207ee8a10b759
Tom2132t <thomakarakashi@gmail.com>
2025-01-18 22:11:52 +0100

    feat(not-found): add page not found component

---
+++ src/app/shared/components/not-found/not-found.component.ts
@@ -0,0 +1,25 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-not-found',
+  standalone: true,
+  imports: [RouterModule],
+  template: `<div
+    class="flex flex-col items-center justify-center h-screen text-center"
+  >
+    <h1 class="text-9xl font-extrabold text-red-500">404</h1>
+    <p class="mt-4 text-2xl font-medium">
+      Oops! The page you're looking for doesn't exist.
+    </p>
+    <a
+      routerLink="/.."
+      class="mt-6 px-6 py-3 text-white bg-blue-600 hover:bg-blue-700 rounded-lg shadow-md focus:ring-4 focus:ring-blue-300"
+    >
+      Go Back Home
+    </a>
+  </div> `,
+  styles: '',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class NotFoundComponent {}
