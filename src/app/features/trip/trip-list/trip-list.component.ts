48d0305392341dbd0f8e38965cf9512be61fc809
Tom2132t <thomakarakashi@gmail.com>
2025-01-19 23:32:36 +0100

    feat(trip-list): implement infinity scroll in the list

--- src/app/features/trip/trip-list/trip-list.component.ts
+++ src/app/features/trip/trip-list/trip-list.component.ts
@@ -6,20 +6,54 @@ import { CommonModule } from '@angular/common';
 import { TripModel } from '../../../shared/models/trip.model';
 import * as TripActions from '../../../store/trip/trip.actions';
 import { AppState } from '../../../store/app.state';
+import { InfiniteScrollDirective } from 'ngx-infinite-scroll';
 
 @Component({
   selector: 'app-trip-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, InfiniteScrollDirective],
   templateUrl: './trip-list.component.html',
-  styleUrl: './trip-list.component.scss'
+  styleUrls: ['./trip-list.component.scss']
 })
 export class TripListComponent implements OnInit {
   private _store = inject(Store<AppState>);
   trips$: Observable<TripModel[]> = this._store.select(selectAllTrips);
 
+  trips: TripModel[] = [];
+  visibleTrips: TripModel[] = [];
+  private _startIndex = 0;
+  readonly batchSize = 5;
+
   ngOnInit(): void {
     this._store.dispatch(TripActions.loadTrips());
+
+    this.trips$
+      .pipe(
+        tap((trips) => {
+          if (trips.length > 0) {
+            this.trips = trips;
+            this._startIndex = 0;
+            this.visibleTrips = [];
+            this.loadMoreTrips();
+          }
+        })
+      )
+      .subscribe();
+  }
+
+  loadMoreTrips(): void {
+    const nextTrips = this.trips.slice(
+      this._startIndex,
+      this._startIndex + this.batchSize
+    );
+    this.visibleTrips = [...this.visibleTrips, ...nextTrips];
+    this._startIndex += this.batchSize;
+  }
+
+  onScroll(): void {
+    if (this._startIndex < this.trips.length) {
+      this.loadMoreTrips();
+    }
   }
 
   onUpdate(trip: TripModel): void {}
