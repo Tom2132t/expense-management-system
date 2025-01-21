58a73171f2283e3ab29ec2988ccb31a5f66a0d2a
Tom2132t <thomakarakashi@gmail.com>
2025-01-21 02:02:12 +0100

    feat(trip-list): implement mat table with pagination and filter

--- src/app/features/trip/trip-list/trip-list.component.ts
+++ src/app/features/trip/trip-list/trip-list.component.ts
@@ -1,26 +1,21 @@
-import {
-  AfterViewInit,
-  Component,
-  inject,
-  OnInit,
-  ViewChild
-} from '@angular/core';
-import { Store } from '@ngrx/store';
-import { Observable, tap } from 'rxjs';
-import { selectAllTrips } from '../../../store/trip/trip.selectors';
 import { CommonModule } from '@angular/common';
-import { TripModel } from '../../../shared/models/trip.model';
-import * as TripActions from '../../../store/trip/trip.actions';
-import { AppState } from '../../../store/app.state';
-import { ToastService } from '../../../core/services/toaster.service';
+import { Component, inject, OnInit, ViewChild } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatTableDataSource, MatTableModule } from '@angular/material/table';
-import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
-import { MatSort, MatSortModule } from '@angular/material/sort';
 import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
-import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatPaginator, MatPaginatorModule } from '@angular/material/paginator';
+import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
+import { Store } from '@ngrx/store';
+import { Observable, tap } from 'rxjs';
+import { ToastService } from '../../../core/services/toaster.service';
+import { TripModel } from '../../../shared/models/trip.model';
+import { AppState } from '../../../store/app.state';
+import * as TripActions from '../../../store/trip/trip.actions';
+import { selectAllTrips } from '../../../store/trip/trip.selectors';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-trip-list',
@@ -39,12 +34,13 @@ import { MatFormFieldModule } from '@angular/material/form-field';
   templateUrl: './trip-list.component.html',
   styleUrls: ['./trip-list.component.scss']
 })
-export class TripListComponent implements OnInit, AfterViewInit {
+export class TripListComponent implements OnInit {
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
 
   private _store = inject(Store<AppState>);
   private _toasterService = inject(ToastService);
+  private _router = inject(Router);
 
   displayedColumns: string[] = [
     'name',
@@ -54,11 +50,14 @@ export class TripListComponent implements OnInit, AfterViewInit {
     'totalExpense',
     'actions'
   ];
+
   dataSource = new MatTableDataSource<TripModel>();
 
   trips$: Observable<TripModel[]> = this._store.select(selectAllTrips).pipe(
     tap((res) => {
-      this.dataSource.data = res;
+      this.dataSource = new MatTableDataSource(res);
+      this.dataSource.paginator = this.paginator;
+      this.dataSource.sort = this.sort;
     })
   );
 
@@ -66,12 +65,9 @@ export class TripListComponent implements OnInit, AfterViewInit {
     this._loadTrips();
   }
 
-  ngAfterViewInit(): void {
-    this.dataSource.paginator = this.paginator;
-    this.dataSource.sort = this.sort;
+  onUpdate(id: TripModel): void {
+    this._router.navigate(['/trip', id]);
   }
-
-  onUpdate(trip: TripModel): void {}
 
   onDelete(id: string): void {
     if (confirm('Are you sure you want to delete this trip?')) {
@@ -94,5 +90,6 @@ export class TripListComponent implements OnInit, AfterViewInit {
 
   private _loadTrips(): void {
     this._store.dispatch(TripActions.loadTrips());
+    this.trips$.subscribe();
   }
 }
