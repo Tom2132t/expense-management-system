5d319c7bcbb8aa3ec13458bf437203009ef925b7
Tom2132t <thomakarakashi@gmail.com>
2025-01-23 02:45:14 +0100

    feat(header): add logout button

--- src/app/core/layout/header/header.component.ts
+++ src/app/core/layout/header/header.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { AuthService } from '../../../auth/auth.service';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule, MatIconModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  authService = inject(AuthService);
+  private _router = inject(Router);
 
+  onLogout(): void {
+    this.authService.logout();
+    this._router.navigate(['/login']);
+  }
 }
